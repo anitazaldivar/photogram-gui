d88ee403e10c4b3f8cd71d6aa08d4fc576a19837
anitazaldivar <128723674+anitazaldivar@users.noreply.github.com>
2024-05-06 22:40:35 +0000

    show photo progress

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -5,4 +5,12 @@ class PhotosController < ApplicationController
     @list_of_photos = matching_photos.order({ :created_at => :desc})
     render({ :template => "photo_templates/index"})
   end
+
+  def show
+    path = params.fetch("path_photo_id")
+    matching_photos = Photo.where({ :id => path})
+
+    @the_photo = matching_photos.at(0)
+    render({ :template => "photo_templates/show"})
+  end
 end
