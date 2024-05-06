851128721ce751bf771125637012d1dc63f8f15a
anitazaldivar <128723674+anitazaldivar@users.noreply.github.com>
2024-05-06 23:09:27 +0000

    some progress delete photo

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -13,4 +13,14 @@ class PhotosController < ApplicationController
     @the_photo = matching_photos.at(0)
     render({ :template => "photo_templates/show"})
   end
+
+  def delete 
+    path = params.fetch("path_photo_id")
+    matching_photos = Photo.where({ :id => path})
+
+    @the_photo = matching_photos.at(0)
+    @the_photo.destroy
+    redirect_to("/photos", { :notice => "Photo deleted successfully."} )
+    #render({ :template => "photo_templates/delete"})
+  end
 end
